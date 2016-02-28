afb20d7384aa68eaa92500d940a16acd460f816d
Luiz Branco <me@luizbranco.com>
2016-02-28 02:38:38 -0500

    Js working example

--- main.go
+++ main.go
@@ -1,28 +1,58 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/google/flatbuffers/go"
+	"github.com/luizbranco/invasion/internal/protocol/client"
 	"github.com/luizbranco/invasion/internal/views"
 
 	"golang.org/x/net/websocket"
 )
 
-func echo(ws *websocket.Conn) {
+func server(ws *websocket.Conn) {
+	for {
+		var data []byte
+		err := websocket.Message.Receive(ws, &data)
+		if err != nil {
+			break
+		}
+		msg := client.GetRootAsMessage(data, 0)
 
+		unionTable := new(flatbuffers.Table)
+
+		if msg.Request(unionTable) {
+			switch msg.RequestType() {
+			case client.RequestCreateAccount:
+				acc := &client.CreateAccount{}
+				acc.Init(unionTable.Bytes, unionTable.Pos)
+				fmt.Printf("Joined %s\n", acc.Name())
+			default:
+				log.Fatal("Unknown type")
+			}
+		} else {
+			log.Fatalf("No type %v", data)
+		}
+	}
 }
 
 func main() {
 	fs := http.FileServer(http.Dir("assets"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
-	http.Handle("/ws", websocket.Handler(echo))
+	http.Handle("/ws", websocket.Handler(server))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "GET" {
 			views.Render(w, "index", nil)
+		} else {
+			http.Error(w, "", http.StatusMethodNotAllowed)
 		}
 	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 /*
