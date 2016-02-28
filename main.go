e1fca49652b7d8b33847c8b9b99e6d033801629a
Luiz Branco <me@luizbranco.com>
2016-02-28 05:00:08 -0500

    Echo chat example

--- main.go
+++ main.go
@@ -7,12 +7,13 @@ import (
 
 	"github.com/google/flatbuffers/go"
 	"github.com/luizbranco/invasion/internal/protocol/client"
+	"github.com/luizbranco/invasion/internal/protocol/server"
 	"github.com/luizbranco/invasion/internal/views"
 
 	"golang.org/x/net/websocket"
 )
 
-func server(ws *websocket.Conn) {
+func handler(ws *websocket.Conn) {
 	for {
 		var data []byte
 		err := websocket.Message.Receive(ws, &data)
@@ -29,6 +30,24 @@ func server(ws *websocket.Conn) {
 				acc := &client.CreateAccount{}
 				acc.Init(unionTable.Bytes, unionTable.Pos)
 				fmt.Printf("Joined %s\n", acc.Name())
+			case client.RequestChatMessage:
+				chat := &client.ChatMessage{}
+				chat.Init(unionTable.Bytes, unionTable.Pos)
+				builder := flatbuffers.NewBuilder(0)
+
+				txt := builder.CreateString(string(chat.Message()))
+				server.ChatMessageStart(builder)
+				server.ChatMessageAddMessage(builder, txt)
+				resp := client.ChatMessageEnd(builder)
+
+				server.MessageStart(builder)
+				server.MessageAddResponseType(builder, server.ResponseChatMessage)
+				server.MessageAddResponse(builder, resp)
+				msg := client.MessageEnd(builder)
+
+				builder.Finish(msg)
+
+				websocket.Message.Send(ws, builder.FinishedBytes())
 			default:
 				log.Fatal("Unknown type")
 			}
@@ -42,7 +61,7 @@ func main() {
 	fs := http.FileServer(http.Dir("assets"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
-	http.Handle("/ws", websocket.Handler(server))
+	http.Handle("/ws", websocket.Handler(handler))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "GET" {
