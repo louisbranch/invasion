d70b2a6989c5cb20c33ac4fc8c4cfa3384a0dc9e
Luiz Branco <me@luizbranco.com>
2016-03-02 00:31:21 -0500

    Delay template rendering

---
+++ assets/js/response.js
@@ -0,0 +1,45 @@
+function Response() {
+  this._builder = new flatbuffers.Builder(0);
+
+};
+
+Response.prototype.createAccount = function(name, email) {
+  var b = this._builder;
+  var name = b.createString(name);
+  var email = b.createString(email);
+  client.CreateAccount.startCreateAccount(b);
+  client.CreateAccount.addName(b, name);
+  client.CreateAccount.addEmail(b, email);
+  var req = client.CreateAccount.endCreateAccount(b);
+  return this._finish(req, client.Request.CreateAccount);
+}
+
+Response.prototype.createGame = function() {
+  var b = this._builder;
+  client.CreateGame.startCreateGame(b);
+  var req = client.CreateGame.endCreateGame(b);
+  return this._finish(req, client.Request.CreateGame);
+};
+
+Response.prototype.joinGame = function() {
+
+};
+
+Response.prototype.createChatMessage = function(content) {
+  var b = this._builder;
+  var txt = b.createString(content);
+  client.ChatMessage.startChatMessage(b);
+  client.ChatMessage.addMessage(b, txt);
+  var req = client.ChatMessage.endChatMessage(b);
+  return this._finish(req, client.Request.ChatMessage);
+};
+
+Response.prototype._finish = function (req, type) {
+  var b = this._builder;
+  client.Message.startMessage(b);
+  client.Message.addRequestType(b, type);
+  client.Message.addRequest(b, req);
+  var msg = client.Message.endMessage(b);
+  b.finish(msg);
+  return b.asUint8Array();
+}
--- assets/js/views.js
+++ assets/js/views.js
@@ -2,20 +2,16 @@ var game = new Game();
 
 Vue.config.delimiters = ['${', '}'];
 
-new Vue({
-  el: '#chat',
+var ChatView = Vue.extend({
   init: function() {
     var view = this;
     game.events.on('chat.message', function(msg) {
       view.log.push({name: 'Luiz', content: msg});
     });
   },
-  data: {
-    log: [],
-    msg: ''
-  },
   methods: {
     send: function() {
+      console.log('send');
       var res = new Response();
       res.createChatMessage(this.msg);
       this.log.push({name: 'luiz', content: this.msg});
@@ -23,3 +19,14 @@ new Vue({
     }
   }
 });
+
+game.events.on('join.game', function() {
+  var chat = new ChatView({
+    el: '#chat',
+    data: {
+      log: [],
+      msg: ''
+    }
+  });
+});
+
